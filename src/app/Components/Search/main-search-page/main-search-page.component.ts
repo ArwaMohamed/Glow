f1fa8ec8ff0f133f47a69532ddaaacec1f75f726
unknown <arwamohamed44.am@gmail.com>
2022-05-18 13:52:28 +0200

    create app

---
+++ src/app/Components/Search/main-search-page/main-search-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-search-page',
+  templateUrl: './main-search-page.component.html',
+  styleUrls: ['./main-search-page.component.css']
+})
+export class MainSearchPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
